6de9f0f77d5d48ddaeaf20efe2398534eed7d931
Tatu Maunu <tatu.maunu@aalto.fi>
2023-11-28 11:37:25 +0200

    Refined About.tsx text

--- src/Components/About.tsx
+++ src/Components/About.tsx
@@ -4,7 +4,7 @@ const About = () => {
     return (
         <section className={'hidden'}>
             <h1 className={'aboutMeHeader'}>About me:</h1>
-            <p className={'aboutMeText'}>I am Tatu, a 2nd year computer science student interested in bridging technology and business. My joy in programming grows from the solutions it can provide to offer real impact.
+            <p className={'aboutMeText'}>I am Tatu, a computer science student and software engineer interested in bridging technology and business. My joy in programming grows from the solutions it can provide to offer real impact.
                 Merging analytical problem solving with creative multidisciplinary teamwork gets me excited. Scroll further down to learn about my expertise and experience</p>
         </section>
     )
