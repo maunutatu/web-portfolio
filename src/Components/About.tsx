d589bc3f67dca7f24dcc4e34c494aba9fbc1d3a3
Tatu Maunu <tatu.maunu@aalto.fi>
2023-12-09 13:36:11 +0200

    Modified About.tsx text content to align better with my desired output

--- src/Components/About.tsx
+++ src/Components/About.tsx
@@ -4,8 +4,8 @@ const About = () => {
     return (
         <section className={'hidden'}>
             <h1 className={'aboutMeHeader'}>About me:</h1>
-            <p className={'aboutMeText'}>I am Tatu, a computer science student and software engineer interested in bridging technology and business. My joy in programming grows from the solutions it can provide to offer real impact.
-                Merging analytical problem solving with creative multidisciplinary teamwork gets me excited. Scroll further down to learn about my expertise and experience</p>
+            <p className={'aboutMeText'}>I am Tatu, a computer science student and software engineer interested in leveraging technology to yield maximum impact. My work experience to-date has mostly been full stack web development.
+                Merging analytical problem solving with creative multidisciplinary teamwork gets me excited. Scroll further down to learn about my technical expertise.</p>
         </section>
     )
 }
