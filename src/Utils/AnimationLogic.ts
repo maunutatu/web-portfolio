260f91cf37012be35519355fbaa716bff96a1068
Tatu Maunu <tatu.maunu@aalto.fi>
2024-10-20 10:23:37 +0300

    Allow transitioning between all animations

--- src/Utils/AnimationLogic.ts
+++ src/Utils/AnimationLogic.ts
@@ -6,23 +6,38 @@ const AnimationLogic = (animationRef: React.RefObject<any>): void => {
   const sectionForKickingAnimation: Element | null = document.querySelector('.otherText')
   const sectionForStandingAnimation: Element | null = document.querySelector('.contactText')
 
+  // Referencing the animation directly causes problems. Have to use this string ref to work around it.
+  const getCurrentAnimation = () => {
+    const { currentAnimationRef, standAnimation, waveAnimation, textAnimation, kickAnimation } = animationRef.current
+    const currentAnimation = currentAnimationRef.current
+    switch (currentAnimation) {
+    case 'stand':
+      return standAnimation
+    case 'wave':
+      return waveAnimation
+    case 'text':
+      return textAnimation
+    case 'kick':
+      return kickAnimation
+    }
+  }
+
   const observer: IntersectionObserver = new IntersectionObserver((entries: IntersectionObserverEntry[]): void => {
     entries.forEach((entry: IntersectionObserverEntry): void => {
       if (entry.isIntersecting) {
         const { currentAnimationRef, standAnimation, waveAnimation, textAnimation, kickAnimation, mixer } = animationRef.current
-        const currentAnimation = currentAnimationRef.current
         switch (entry.target.className) {
         case 'nameHeader':
-          waveAnimation.reset().crossFadeFrom(currentAnimation === 'stand' ? standAnimation : textAnimation, 0.5, true).play()
+          waveAnimation.reset().crossFadeFrom(getCurrentAnimation(), 0.5, true).play()
           currentAnimationRef.current = 'wave'
           break
         case 'aboutMeText':
-          textAnimation.reset().crossFadeFrom(currentAnimation === 'wave' ? waveAnimation : standAnimation, 0.5, true).play()
+          textAnimation.reset().crossFadeFrom(getCurrentAnimation(), 0.5, true).play()
           currentAnimationRef.current = 'text'
           break
         case 'otherText':
-          if(currentAnimation === 'kick') return
-          kickAnimation.reset().crossFadeFrom(currentAnimation === 'text' ? textAnimation : standAnimation, 0.5, true).setLoop(LoopRepeat, 2).play()
+          if(currentAnimationRef.current === 'kick') return
+          kickAnimation.reset().crossFadeFrom(getCurrentAnimation(), 0.5, true).setLoop(LoopRepeat, 2).play()
           currentAnimationRef.current = 'kick'
           mixer.addEventListener('finished', () => {
             standAnimation.reset().play()
