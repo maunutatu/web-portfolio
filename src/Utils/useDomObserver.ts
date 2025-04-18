6bab60ca9f63d57b75bdc751a83c6a1405c95599
Tatu Maunu <tatu.maunu@hoxhunt.com>
2025-04-18 15:18:37 +0300

    Fix mutation observer not firing on macOS

--- src/Utils/useDomObserver.ts
+++ src/Utils/useDomObserver.ts
@@ -3,17 +3,22 @@ import AnimationLogic from './AnimationLogic'
 import { TAnimationControls } from '../App'
 import { RefObject, useEffect } from 'react'
 
-
 const useDOMObserver = (animationRef: RefObject<TAnimationControls>): void => {
   useEffect(() => {
-    const mutationObserver: MutationObserver = new MutationObserver((_entries: MutationRecord[], observer: MutationObserver): void => {
-      const hiddenSections: NodeListOf<Element> = document.querySelectorAll('.hidden')
-      AnimationLogic(animationRef)
-      OpacityLogic(hiddenSections)
-      observer.disconnect()
-    })
+    const mutationObserver: MutationObserver = new MutationObserver(
+      (_entries: MutationRecord[], observer: MutationObserver): void => {
+        const hiddenSections: NodeListOf<Element> =
+          document.querySelectorAll('.hidden')
+        AnimationLogic(animationRef)
+        OpacityLogic(hiddenSections)
+        observer.disconnect()
+      }
+    )
 
-    mutationObserver.observe(document.body, { childList: true, subtree: true })
+    mutationObserver.observe(document.body, {
+      subtree: true,
+      attributes: true,
+    })
   }, [])
 }
 
